469c6ea3f26fc7f8f7179910b1bdc11af18eddb6
Kotelnikov Denis <slammy1101@mail.ru>
2017-06-30 11:33:27 +0300

    Initial commit

---
+++ src/BeatBoxTestDrive.java
@@ -0,0 +1,9 @@
+/**
+ * Created by SuperDenissio on 30.06.2017.
+ */
+public class BeatBoxTestDrive {
+    public static void main(String[] args) {
+        BeatBox box = new BeatBox();
+        box.buildGUI();
+    }
+}
